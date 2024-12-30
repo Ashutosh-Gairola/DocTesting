e35a152435e65d2a086b3cc86693905b43a34ddc
Ashutosh Gairola <ashutoshgairola2003@gmail.com>
2024-12-30 13:39:29 +0530

    Add project code and documenttion

---
+++ calculator/__init__.py
@@ -0,0 +1,12 @@
+# calculator/__init__.py
+
+# docstring of the calculator package
+
+"""Do math with your own functions.
+
+Modules exported by this package:
+
+- `calculations`: Provide several sample math calculations.
+"""
+
+
